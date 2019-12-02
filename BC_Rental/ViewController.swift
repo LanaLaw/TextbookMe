04cde19fd8609e1548251f74b8de36796e58bf7a
Ellana Lawrence <elawrence.blain@gmail.com>
2019-12-02 16:54:37 -0500

    Initial Commit

---
+++ BC_Rental/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BC_Rental
+//
+//  Created by Ellana Lawrence on 12/2/19.
+//  Copyright © 2019 Ellana Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
